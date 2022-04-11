ad40cb88f0a7057fe86496398d33a735adef3f27
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2022-04-11 11:33:56 +0300

    separate pharmaone from hs

--- src/pharmaone/src/Pharmacy/Sections/Configuration/Configuration.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Configuration.css";
-
-const Configuration = () => {
-  return <div>Configuration</div>;
-};
-
-export default Configuration;
--- src/pharmaone/src/Pharmacy/Sections/Covid/Covid.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Covid.css";
-
-const Covid = () => {
-  return <div>Covid</div>;
-};
-
-export default Covid;
