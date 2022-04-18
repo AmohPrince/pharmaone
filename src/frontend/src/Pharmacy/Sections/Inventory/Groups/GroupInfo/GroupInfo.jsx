9cf85e4b7c50af18782fe25afc159a93aeef8a0a
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2022-04-18 12:51:09 +0300

    Checkpoint ✅

--- src/frontend/src/Pharmacy/Sections/Inventory/Groups/GroupInfo/GroupInfo.jsx
+++ src/frontend/src/Pharmacy/Sections/Inventory/Groups/GroupInfo/GroupInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "./GroupInfo.css";
 import { RedButton, SectionName } from "../../../../Components/Components";
 import { useParams } from "react-router-dom";
@@ -9,9 +9,14 @@ import { Searchbar } from "../../../../Components/Components";
 
 const GroupInfo = () => {
   const [modalState, setModalState] = useState(false);
+  const [groupOverlay, setGroupOverlay] = useState(true);
   let params = useParams();
   const incomingData = useContext(dataFlowContext);
   const data = incomingData.getSpecificGroupWithName(params.groupName);
+
+  useEffect(() => {
+    setGroupOverlay((prevState) => !prevState);
+  }, [modalState]);
 
   const title = {
     main: `${data.groupName}(${data.noOfMedicine})`,
@@ -65,52 +70,58 @@ const GroupInfo = () => {
   };
 
   return (
-    <div className="Inventory__container Group__info">
-      <div className="flex__container Group__info-top">
-        <SectionName title={title} />
-        <div className="group__info-modal" onClick={handleModal}>
-          <RedButton buttonData={buttonData} />
+    <>
+      <div className={`Group__overlay ${groupOverlay}`}></div>
+      <div className="Inventory__container Group__info">
+        <div className="flex__container Group__info-top">
+          <SectionName title={title} />
+          <div className="group__info-modal" onClick={handleModal}>
+            <RedButton buttonData={buttonData} />
+          </div>
         </div>
+        <Searchbar data={searchBarData} />
+        <div>
+          <div className="Group__container-titles flex__container">
+            <div>
+              <p className="p__poppins">Medicine Name</p>
+              <img src={Assets.TopBottomArrows} alt="Arrows" />
+            </div>
+            <div>
+              <p className="p__poppins">No Of Medicines</p>
+              <img src={Assets.TopBottomArrows} alt="Arrows" />
+            </div>
+            <div>
+              <p className="p__poppins">Action</p>
+            </div>
+          </div>
+          <div className="splitter" />
+          {mockGroupMedicines.map((data) => {
+            return (
+              <SingleMedicineInGroup data={data} key={data.medicineName} />
+            );
+          })}
+        </div>
+
+        <RedButton buttonData={buttonData2} />
+        {modalState === true ? (
+          <div className="modal__wrapper">
+            <div className="modal">
+              <div className="modal__title">
+                <p className="p__poppins">Add Medicine</p>
+              </div>
+              <div className="modal__subtitle">
+                <p className="p__poppins">Medicine</p>
+                <Searchbar data={searchBarData2} />
+              </div>
+              <RedButton buttonData={buttonData3} />
+              <div className="closemodal" onClick={handleModal}>
+                <img src={Assets.Close} alt="Close" />
+              </div>
+            </div>
+          </div>
+        ) : null}
       </div>
-      <Searchbar data={searchBarData} />
-      <div>
-        <div className="Group__container-titles flex__container">
-          <div>
-            <p className="p__poppins">Medicine Name</p>
-            <img src={Assets.TopBottomArrows} alt="Arrows" />
-          </div>
-          <div>
-            <p className="p__poppins">No Of Medicines</p>
-            <img src={Assets.TopBottomArrows} alt="Arrows" />
-          </div>
-          <div>
-            <p className="p__poppins">Action</p>
-          </div>
-        </div>
-        <div className="splitter" />
-        {mockGroupMedicines.map((data) => {
-          return <SingleMedicineInGroup data={data} key={data.medicineName} />;
-        })}
-      </div>
-      <RedButton buttonData={buttonData2} />
-      {modalState === true ? (
-        <div className="modal__wrapper">
-          <div className="modal">
-            <div className="modal__title">
-              <p className="p__poppins">Add Medicine</p>
-            </div>
-            <div className="modal__subtitle">
-              <p className="p__poppins">Medicine</p>
-              <Searchbar data={searchBarData2} />
-            </div>
-            <RedButton buttonData={buttonData3} />
-            <div className="closemodal" onClick={handleModal}>
-              <img src={Assets.Close} alt="Close" />
-            </div>
-          </div>
-        </div>
-      ) : null}
-    </div>
+    </>
   );
 };
 
