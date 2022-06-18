85720d419f003da51502fdffb06cb44b92bfb5bc
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2022-06-18 22:36:09 +0300

    I hate stack overflow

--- src/frontend/src/Pharmacy/Sections/ContactManagement/ContactManagement.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./ContactManagement.css";
-
-const ContactManagement = () => {
-  return <div>ContactManagement</div>;
-};
-
-export default ContactManagement;
--- src/frontend/src/Pharmacy/Sections/TechHelp/TechHelp.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./TechHelp.css";
-
-const TechHelp = () => {
-  return <div>TechHelp</div>;
-};
-
-export default TechHelp;
